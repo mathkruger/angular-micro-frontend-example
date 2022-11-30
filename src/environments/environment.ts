df46c3e2c9da6cab56ee8c83627b09dfa3ee76f3
Matheus da Silva Kruger <matheusk@ciandt.com>
2022-11-30 14:38:14 -0300

    using the micro apps from local env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,17 @@ export const environment = {
   microApps: [
     {
       tagName: "micro-app-order-list",
-      url: "https://matheuskrugerdev.cf/angular-micro-frontend-example/order-list/package.js",
+      url: "http://localhost:5000/order-list/package.js",
       autoRender: true
     },
     {
       tagName: "micro-app-material-list",
-      url: "https://matheuskrugerdev.cf/angular-micro-frontend-example/material-list/package.js",
+      url: "http://localhost:5000/material-list/package.js",
       autoRender: true
     },
     {
       tagName: "micro-app-homepage",
-      url: "https://matheuskrugerdev.cf/angular-micro-frontend-example/homepage/package.js",
+      url: "http://localhost:5000/homepage/package.js",
       autoRender: true
     }
   ]
