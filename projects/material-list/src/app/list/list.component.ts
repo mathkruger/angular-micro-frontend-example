04b2d3119c29c1b684846b99d927787defbf4916
Matheus da Silva Kruger <matheusk@ciandt.com>
2022-11-30 13:46:56 -0300

    adding styling to material list

--- projects/material-list/src/app/list/list.component.ts
+++ projects/material-list/src/app/list/list.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  materials: any[] = [
+    {
+      id: 1,
+      name: "Material 1",
+      warehouse: "Warehouse 1"
+    },
+    {
+      id: 2,
+      name: "Material 2",
+      warehouse: "Warehouse 1"
+    },
+    {
+      id: 3,
+      name: "Material 3",
+      warehouse: "Warehouse 1"
+    },
+    {
+      id: 4,
+      name: "Material 4",
+      warehouse: "Warehouse 1"
+    },
+    {
+      id: 5,
+      name: "Material 5",
+      warehouse: "Warehouse 1"
+    },
+    {
+      id: 6,
+      name: "Material 6",
+      warehouse: "Warehouse 1"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
